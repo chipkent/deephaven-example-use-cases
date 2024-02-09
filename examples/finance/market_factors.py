0b0b2e9ddadc86b05c590b70a39c0037d479c5a3
Chip Kent <chipkent@deephaven.io>
2024-02-09 12:37:22 -0700

    Creating a market factors example.

--- examples/finance/market_factors.py
+++ examples/finance/market_factors.py
@@ -1,4 +1,54 @@
+from typing import Sequence, Tuple
+import numpy as np
+import numpy.typing as npt
 from deephaven.numpy import to_numpy
+from sklearn.decomposition import PCA
+from deephaven.table import Table
+from deephaven.table_factory import new_table
+from deephaven.column import string_col, double_col
+
+
+def compute_factors(prices: Table, times: Table, symbols: Sequence[str], n_components: int) -> Tuple[Table, npt.NDArray[np.float64], npt.NDArray[np.float64]]:
+    """ Compute the PCA factors for the given symbols and times.
+
+    Args:
+        prices: A table containing the price data for the given symbols.  Must contain columns "Timestamp", and "Sym".
+        times: A table containing the times to compute the factors.  Must contain a column "Timestamp".
+        symbols: A sequence of symbols to compute the factors for.
+        n_components: The number of components to compute.
+
+    Returns:
+        A table containing the computed factors, a numpy array of the explained variance ratios, and a numpy array of the cumulative explained variance ratios.
+    """
+    prices_wide = times.view("Timestamp")
+
+    for sym in symbols:
+        prices_wide = prices_wide.aj(prices.where("Sym == sym"), ["Timestamp"], f"{sym}=Price")
+
+    deltas = prices_wide \
+        .drop_columns("Timestamp") \
+        .select([f"{sym} = log({sym}) - log({sym}_[ii-1])" for sym in symbols]) \
+        .where([f"isFinite({sym})" for sym in symbols])
+
+    returns = to_numpy(deltas)
+
+    pca = PCA(n_components=n_components)
+    pca.fit(returns)
+    pca_components = pca.components_
+
+    betas = new_table(
+        [string_col("Sym", symbols)] +
+        [double_col(f"Factor{i}", pca_components[i]) for i in range(n_components)]
+    )
+
+    pct = pca.explained_variance_ratio_
+    cum_pct = np.cumsum(pct)
+
+    return betas, pct, cum_pct
+
+
+
+## Example usage on Deephaven Enterprise
 
 date_min = "2024-02-01"
 date_max = "2024-02-08"
@@ -6,12 +56,8 @@ n_components = 4
 symbols = [
     'IBM',
     'MSFT',
-    # 'META',
     'INTC',
     'NEM',
-    # 'AU',
-    # 'AEM',
-    # 'GFI'
     'BA',
     'GOOG',
     'CAT',
@@ -19,9 +65,9 @@ symbols = [
     'AAPL',
     'AMZN',
     'NVDA',
-    # 'TSLA',
     'LLY',
-    'V'
+    'V',
+    'XOM',
 ]
 
 import deephaven.dtypes as dht
@@ -34,31 +80,5 @@ trades = db.historical_table("FeedOS", "EquityTradeL1") \
 
 times = trades.view("Timestamp=lowerBin(Timestamp,'PT00:10:00'.toNanos())").select_distinct().sort("Timestamp")
 
-prices_wide = times
-
-for sym in symbols:
-    prices_wide = prices_wide.aj(trades.where("Sym == sym"), "Timestamp", f"{sym}=Price")
-
-deltas = prices_wide \
-    .drop_columns("Timestamp") \
-    .select([f"{sym} = log({sym}) - log({sym}_[ii-1])" for sym in symbols]) \
-    .where([f"isFinite({sym})" for sym in symbols])
-
-returns = to_numpy(deltas)
-
-from sklearn.decomposition import PCA
-import pandas as pd
-from deephaven.pandas import to_table
-
-# Fit PCA model
-pca = PCA(n_components=n_components)
-pca.fit(returns)
-pca_components = pca.components_
-
-from deephaven.table_factory import new_table
-from deephaven.column import string_col, double_col
-
-betas = new_table(
-    [string_col("Sym", symbols)] + [double_col(f"Factor{i}", pca_components[i]) for i in range(n_components)]
-)
+betas, pct, cum_pct = compute_factors(trades, times, symbols, n_components)
 
