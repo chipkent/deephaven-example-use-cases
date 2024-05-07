7919d0c270b959e4d7ca3cbc9403340c7c311638
Chip Kent <5250374+chipkent@users.noreply.github.com>
2024-05-07 09:53:11 -0600

    New Example: An interface to combine historical and live table access (#5)
    * Combined live and historical table
    
    * Combined live and historical table
    
    * Combined live and historical table
    
    * Reworked the combined table to work for both server and client tables.
    
    * Tested the server implementation.
    
    * Tests the client implementation.  Not run yet.
    
    * Addressing review comments
    
    * Improved testing to be consistent with recent changes.
    
    * Review changes
    
    * Fixed a bug in the table id.
    
    * Bug fix
    
    * Make tests work on the client and server
    
    * Fix buggy test
    
    * Clean up test example
    
    * Avoid table collisions.
    
    * Fix the test script
    
    * Added a readme
    
    * format the test

---
+++ examples/misc/combined_table/combined_table_common.py
@@ -0,0 +1,205 @@
+
+""" A package that provides a CombinedTable class that combines a historical and live table into a single table.
+
+This class can be used with both the Deephaven client and server.
+"""
+import inspect
+from typing import Union, Sequence, Optional, Callable, Generic, TypeVar
+
+Table = TypeVar('Table')
+""" The type of table.  
+By making this a type variable, we can use the CombinedTable class on either the client or server."""
+
+
+class CombinedTable(Generic[Table]):
+    """ A class that combines a historical and live table into a single table.
+    This class is used to provide a single interface to both tables.
+
+    Overrides of table methods are provided to optimize performance.
+    If a performance optimizing override is not provided, the method call is delegated to the combined table.
+    """
+
+    def __init__(self,
+                 merge: Callable[[Sequence[Table]], Table],
+                 hist: Table,
+                 live: Table,
+                 hist_filters: Sequence[str] = None,
+                 live_filters: Sequence[str] = None,
+                 ):
+        """ Create a CombinedTable object.
+
+        Args:
+            merge: The function to merge tables.
+            hist: The historical table.
+            live: The live table.
+            hist_filters: The filters to apply to the historical table.
+            live_filters: The filters to apply to the live table.
+        """
+        self._merge = merge
+        self._historical_raw = hist
+        self._live_raw = live
+        self._hist_filters = hist_filters
+        self._live_filters = live_filters
+        self._historical = None
+        self._live = None
+        self._combined = None
+
+    @property
+    def historical(self) -> Table:
+        """ The historical table."""
+        if not self._historical:
+            self._historical = self._historical_raw.where(self._hist_filters) \
+                if self._hist_filters else self._historical_raw
+
+        return self._historical
+
+    @property
+    def live(self) -> Table:
+        """ The live table."""
+        if not self._live:
+            self._live = self._live_raw.where(self._live_filters) \
+                if self._live_filters else self._live_raw
+
+        return self._live
+
+    @property
+    def combined(self) -> Table:
+        """ The combined table."""
+        if not self._combined:
+            self._combined = self._merge([self.historical, self.live])
+
+        return self._combined
+
+    @property
+    def apply(self) -> 'CombinedTable':
+        """ The CombinedTable with the filters fully applied."""
+        return CombinedTable(self._merge, self.historical, self.live)
+
+    @staticmethod
+    def _args_to_table_decorator(fn: Callable) -> Callable:
+        """ Decorator to convert function arguments from CombinedTable to Table objects."""
+
+        nargs = len(inspect.signature(fn).parameters)
+
+        if nargs == 0:
+            return fn
+
+        def wrapper(*args, **kwargs):
+            args_new = [arg.combined if isinstance(arg, CombinedTable) else arg for arg in args]
+            return fn(*args_new, **kwargs)
+
+        return wrapper
+
+    # If the method doesn't exist in this object, delegate it to the combined table.
+    # Methods are called with other CombinedTable objects as arguments, the arguments are converted to Table objects.
+    def __getattr__(self, attr):
+        c = self.combined
+        a = getattr(c, attr)
+
+        # This is to handle @property methods and properties
+        if not callable(a):
+            return a
+
+        return CombinedTable._args_to_table_decorator(a)
+
+    @staticmethod
+    def _combine_filters(filters1: Union[str, Sequence[str]], filters2: Union[str, Sequence[str]]) -> Optional[Sequence[str]]:
+        """ Combine two sets of filters. If either filter is None, the other filter is returned."""
+
+        if isinstance(filters1, str):
+            filters1 = [filters1]
+
+        if isinstance(filters2, str):
+            filters2 = [filters2]
+
+        if not filters1 and not filters2:
+            return None
+        elif not filters1:
+            return filters2
+        elif not filters2:
+            return filters1
+        else:
+            return list(filters1) + list(filters2)
+
+    def where(self,
+              filters: Union[str, Sequence[str]] = None,
+              apply: bool = True,
+              ) -> 'CombinedTable':
+        """Applies the :meth:`~Table.where` table operation to the combined table, and produces a new CombinedTable.
+
+        Args:
+            filters (Union[str, Sequence[str]], optional): the filter condition
+                expression(s), default is None
+            apply (bool, optional): whether to immediately apply the filters or defer the application until later,
+                default is True
+
+        Returns:
+            a new CombinedTable object
+
+        Raises:
+            DHError
+        """
+
+        if not filters:
+            return self
+
+        if apply:
+            if self._historical:
+                hist = self.historical.where(filters)
+            else:
+                hist_filters = self._combine_filters(self._hist_filters, filters)
+                hist = self._historical_raw.where(hist_filters)
+
+            if self._live:
+                live = self.live.where(filters)
+            else:
+                live_filters = self._combine_filters(self._live_filters, filters)
+                live = self._live_raw.where(live_filters)
+
+            return CombinedTable(self._merge, hist, live)
+        else:
+            hist_filters = self._combine_filters(self._hist_filters, filters)
+            live_filters = self._combine_filters(self._live_filters, filters)
+
+            return CombinedTable(
+                self._merge,
+                self._historical_raw,
+                self._live_raw,
+                hist_filters=hist_filters,
+                live_filters=live_filters,
+            )
+
+    def where_in(self, filter_table: Union[Table, 'CombinedTable'], cols: Union[str, Sequence[str]]) -> 'CombinedTable':
+        """Applies the :meth:`~Table.where_in` table operation to the combined table,
+        and produces a new CombinedTable."""
+
+        if isinstance(filter_table, CombinedTable):
+            filter_table = filter_table.combined
+
+        return CombinedTable(
+            self._merge,
+            self.historical.where_in(filter_table, cols),
+            self.live.where_in(filter_table, cols)
+        )
+
+    def where_not_in(self, filter_table: Union[Table, 'CombinedTable'], cols: Union[str, Sequence[str]]) -> 'CombinedTable':
+        """Applies the :meth:`~Table.where_not_in` table operation to the combined table,
+        and produces a new CombinedTable."""
+
+        if isinstance(filter_table, CombinedTable):
+            filter_table = filter_table.combined
+
+        return CombinedTable(
+            self._merge,
+            self.historical.where_not_in(filter_table, cols),
+            self.live.where_not_in(filter_table, cols)
+        )
+
+    def where_one_of(self, filters: Union[str, Sequence[str]] = None) -> 'CombinedTable':
+        """Applies the :meth:`~Table.where_one_of` table operation to the combined table,
+        and produces a new CombinedTable."""
+        return CombinedTable(
+            self._merge,
+            self.historical.where_one_of(filters),
+            self.live.where_one_of(filters)
+        )
---
+++ examples/misc/combined_table/test_client.py
@@ -0,0 +1,89 @@
+
+# A script to test combined_table on the client
+
+from combined_table_client import combined_table
+from deephaven_enterprise.client.session_manager import SessionManager
+
+# For connection details, see: https://deephaven.io/enterprise/docs/coreplus/coreplus-python-client/
+
+connection_info = "https://hostname:8123/iris/connection.json"
+session_mgr: SessionManager = SessionManager(connection_info)
+
+# Authenticate
+session_mgr.password("username", "password")
+# -- or --
+# session_mgr.private_key("/path-to-private-key/priv-username.base64.txt")
+
+session = session_mgr.connect_to_new_worker(name=None, heap_size_gb=4.0)
+
+# Begin testing
+ct = combined_table(session, "FeedOS", "EquityQuoteL1")
+ct_live = ct.live
+ct_hist = ct.historical
+ct_comb = ct.combined
+
+# test apply
+cta = ct.apply
+cta_live = cta.live
+cta_hist = cta.historical
+cta_comb = cta.combined
+
+# test unspecified method routing
+print(ct.is_refreshing)
+
+# Test a where without applying
+f1 = ct.where("Date > `2024-04-09`", apply=False)
+fc1 = f1.combined
+h1 = f1.head(3)
+
+# Test another where with applying (historical and live already computed)
+f2 = f1.where("Date < `2024-04-11`")
+fc2 = f2.combined
+h2 = f2.head(3)
+
+# Test another where with applying (historical and live not already computed)
+f2a = ct.where("Date > `2024-04-09`", apply=False).where("Date < `2024-04-11`")
+fc2a = f2a.combined
+h2a = f2a.head(3)
+
+# Test a where on an applied table
+f3 = f2.where("Date > `2024-04-01`")
+fc3 = f3.combined
+h3 = f3.head(3)
+
+# Test where with no filters
+f4 = f3.where()
+fc4 = f4.combined
+h4 = f4.head(3)
+assert(f4 == f3)
+
+# Test where_in with a table
+tf = session.empty_table(1).update("Date=`2024-04-09`")
+f5 = f1.where_in(tf, "Date")
+fc5 = f5.combined
+h5 = f5.head(3)
+
+# Test where_in with a CombinedTable
+f6 = f1.where_in(f3, "Date")
+fc6 = f6.combined
+h6 = f6.head(3)
+
+# Test where_not_in with a table
+tf = session.empty_table(1).update("Date=`2024-04-09`")
+f7 = f1.where_not_in(tf, "Date")
+fc7 = f7.combined
+h7 = f7.head(3)
+
+# Test where_in with a CombinedTable
+f8 = f1.where_not_in(f3, "Date")
+fc8 = f8.combined
+h8 = f8.head(3)
+
+# Not yet supported
+# # Test where_one_of
+# f9 = f1.where_one_of(["Date=`2024-04-09`", "Date=`2024-04-06`"])
+# fc9 = f9.combined
+# h9 = f9.head(3)
+
+# Test materializing input CombinedTables
+f10 = f2.natural_join(f2, ["Date", "Timestamp"], ["JAsk=Ask"])
