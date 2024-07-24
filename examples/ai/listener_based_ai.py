d5fb4cce7c90eb9b53dafa4385a547c23fc971a5
Chip Kent <5250374+chipkent@users.noreply.github.com>
2024-07-24 17:19:01 -0400

    Code cleanup

--- examples/ai/listener_based_ai.py
+++ examples/ai/listener_based_ai.py
@@ -1,6 +1,6 @@
 # This example illustrates how Deephaven table listeners can be used to drive real-time AI calculations
 #
-# In this case, a simple NumPy summation is used for the AI model, but TensorFlow, PyTorch, or any 
+# In this case, a simple NumPy calculation is used for the AI model, but TensorFlow, PyTorch, or any 
 # other AI technologies could be used.
 
 # Imports
@@ -10,6 +10,7 @@ import random
 from deephaven import time_table, dtypes, new_table
 from deephaven.column import datetime_col, string_col, double_col
 from deephaven.table_listener import listen, TableUpdate
+from deephaven.stream import blink_to_append_only
 from deephaven.stream.table_publisher import table_publisher
 
 # Create a table to be used as input
@@ -19,16 +20,16 @@ def gen_label() -> str:
 
 source = time_table("PT0.1S").update(formulas=["Label = gen_label()", "X = sqrt(i)", "Y = -sqrt(sqrt(i))"])
 
-# Define the AI model
-
-def compute_ai_model(features):
-    return np.sum(features, axis=1, keepdims=True)
-
 # Set up the output tables
 
 preds, preds_publisher = table_publisher("AIOutput", {"Timestamp": dtypes.Instant, "Label": dtypes.string, "Pred": dtypes.float64})
 
-preds_history = preds.snapshot_when(trigger_table=preds, stamp_cols=[], history=True)
+preds_history = blink_to_append_only(preds)
+
+# Define the AI model
+
+def compute_ai_model(features):
+    return np.sum(features, axis=1, keepdims=True)
 
 # Create a table listener
 
