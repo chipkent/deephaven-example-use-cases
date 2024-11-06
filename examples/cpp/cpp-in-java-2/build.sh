e389c070c72148cba829991a0cf5da51ff708c2c
Chip Kent <chipkent@deephaven.io>
2024-11-06 10:13:54 -0700

    Trying a manual build from https://github.com/opendiff/java-c-plus-plus/blob/master/manual/readme.md

--- examples/cpp/cpp-in-java-2/build.sh
+++ examples/cpp/cpp-in-java-2/build.sh
@@ -17,13 +17,13 @@ g++ -dynamiclib -o "$OUTPUT_DIR/libmyhello.dylib" ./src/main/cpp/myhello.cpp
 # Compile HelloPreset.java
 javac -cp javacpp.jar -d build src/main/java/org/example/presets/HelloPreset.java
 
-# Generate hello.java and JNI Code
-java -cp javacpp.jar:build org.bytedeco.javacpp.tools.Builder -Dplatform.includepath=src/main/cpp  org.example.presets.HelloPreset -d src/main/java
-javac -cp javacpp.jar:src/main/java -d build/ src/main/java/org/example/hello.java
+## Generate hello.java and JNI Code
+java -cp javacpp.jar:build org.bytedeco.javacpp.tools.Builder -Dplatform.includepath=src/main/cpp  org.example.presets.HelloPreset -d build/src/main/java
+javac -cp javacpp.jar:src/main/java -d build/ build/src/main/java/org/example/hello.java
 java -cp javacpp.jar:build:src/main/java org.bytedeco.javacpp.tools.Builder -Dplatform.includepath=src/main/cpp -Dplatform.linkpath=build/macosx-arm64 org.example.hello -d build/macosx-arm64
 
 # Compile Everything
-javac -cp javacpp.jar:src/main/java -d build/ src/main/java/org/example/*.java
+javac -cp javacpp.jar:src/main/java -d build/ {,build/}src/main/java/org/example/*.java
 
 # Run the Application
 java -Djava.library.path=./build/macosx-arm64/ -cp build:javacpp.jar org.example.Main
